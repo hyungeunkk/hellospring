4500ce989506ddb861f4bfa07e43119751d4db11
“khe” <guddms93@gmail.com>
2025-08-14 15:48:12 +0900

    first commit khe

---
+++ src/main/java/kr/kosa/hello/helloController.java
@@ -0,0 +1,18 @@
+package kr.kosa.hello;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class helloController {
+
+  @GetMapping
+  public Map<String, Object> home(){
+    return Map.of("Message", "Hello World");
+  }
+
+}
